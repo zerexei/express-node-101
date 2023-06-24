4331d517f60171a827d027ddcf679caad6e67daa
Angelo Arcillas <angeloarcillas64@gmail.com>
2023-06-24 23:04:48 +0800

    Add: multer | setup user avatar upload route

--- backend/index.ts
+++ backend/index.ts
@@ -1,17 +1,32 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import multer from 'multer'
+
+
 
 dotenv.config();
 
 const app: Express = express();
+
+const upload = multer({ dest: 'uploads/' })
+
 app.use(express.json())
 
-app.post('/', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
+    return res.send("Hello World!");
+})
+
+app.post('/user-name', (req: Request, res: Response) => {
     if (!req.body?.name) {
         return res.send("Please provide a name");
     }
 
     return res.send(`your name is ${req.body.name}`);
+});
+
+app.post("/users/:id/avatar", upload.single('avatar'), (req: Request, res: Response) => {
+    console.log(req.params?.id, "<+++++++++++>", req.file);
+    return res.send("file received");
 });
 
 const port = process.env.PORT || 3002;
