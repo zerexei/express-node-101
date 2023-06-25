d66dab386da649e2836f3e584224723d6689fc7c
Angelo Arcillas <angeloarcillas64@gmail.com>
2023-06-25 18:49:58 +0800

    Add: test fetch request

--- frontend/src/components/book/BookList.tsx
+++ frontend/src/components/book/BookList.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-const BookList = () => {
-  return (
-    <div>BookList</div>
-  )
+export interface UserType {
+  id: number;
+  name: string;
 }
 
-export default BookList
+export interface BookType {
+  id: number;
+  title: string;
+  //   description: string;
+  //   author: UserType | string;
+}
+
+const BookList = () => {
+  const [books, setBooks] = useState<BookType[]>([]);
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  async function fetchBooks() {
+    // const res = await fetch('/books');
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const books: BookType[] = await res.json();
+    setBooks(books);
+  }
+
+  return (
+    <div>
+      {books.map((book) => (
+        <p key={book.id}>{book.title}</p>
+      ))}
+    </div>
+  );
+};
+
+export default BookList;
